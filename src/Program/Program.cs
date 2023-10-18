8fae4af033ca6f51090bb4dcd5f3dab233bef008
santiagoleites501 <santiagoleites501@gmail.com>
2023-10-18 10:46:53 -0300

    comienso del program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,10 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture("luke.jpg");
+            Pipe.Serial();
+            provider.SavePicture(picture, "lukeSk.jpg");// aplicar secuencia de filtros con pipeserial y el ultimo es pipenul
         }
     }
 }
